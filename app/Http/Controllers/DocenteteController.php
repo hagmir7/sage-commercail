aa354d01d341a70ffd09cfeb09bba5e29c752d38
Agmir Hassane <hagmir6@gmail.com>
2025-05-08 22:44:02 +0100

    New udpates

--- app/Http/Controllers/DocenteteController.php
+++ app/Http/Controllers/DocenteteController.php
@@ -14,7 +14,7 @@ class DocenteteController extends Controller
     public function index($status)
     {
         return DB::select(
-            "SELECT DO_Reliquat, DO_Piece, DO_Ref, DO_Tiers, CONVERT(VARCHAR(10), DO_Date, 111) AS DO_Date, CONVERT(VARCHAR(10), DO_DateLivr, 111) AS DO_DateLivr, DO_Expedit
+            "SELECT DO_Reliquat, DO_Piece, DO_Ref, DO_Tiers, cbMarq, CONVERT(VARCHAR(10), DO_Date, 111) AS DO_Date, CONVERT(VARCHAR(10), DO_DateLivr, 111) AS DO_DateLivr, DO_Expedit
             FROM F_DOCENTETE WHERE DO_Domaine = 0 AND DO_Type = 2 AND DO_Statut = 1"
         );
     }
