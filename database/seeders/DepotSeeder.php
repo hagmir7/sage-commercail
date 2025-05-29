a03d174955f25661aa4472ae798df2dfe8b87466
Agmir Hassane <hagmir6@gmail.com>
2025-05-29 22:27:50 +0100

    Add Table sseed

---
+++ database/seeders/DepotSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DepotSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('depots')->insert([
+            ['code' => 'DEPOT 1', 'company_id' => 1],
+            ['code' => 'DEPOT 2', 'company_id' => 1],
+            ['code' => 'DEPOT 3', 'company_id' => 1],
+            ['code' => 'DEPOT 4', 'company_id' => 1],
+            ['code' => 'DEPOT 5', 'company_id' => 1],
+            ['code' => 'DEPOT 6', 'company_id' => 1],
+            ['code' => 'DEPOT 7', 'company_id' => 1],
+            ['code' => 'DEPOT 8', 'company_id' => 1],
+            ['code' => 'DEPOT 9', 'company_id' => 1],
+            ['code' => 'DEPOT 10', 'company_id' => 1],
+            ['code' => 'FABRICA', 'company_id' => 1],
+            ['code' => 'TRANSFERT EXTERNE', 'company_id' => 3],
+            ['code' => 'SERIE MOBLE', 'company_id' => 1],
+            ['code' => 'DEPOT SM 1', 'company_id' => 2],
+        ]);
+    }
+}
